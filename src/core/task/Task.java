efaa23d2a42ecbcec5563edb5be6bced50aeb919
asermax <asermax@gmail.com>
2012-08-03 01:35:11 -0300

    Agreagados comments y modificado el método toString de las clases Task y Hour para que sean mas relevantes

--- src/core/task/Task.java
+++ src/core/task/Task.java
@@ -1,56 +1,104 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package core.task;
 
 import core.utils.Hour;
 
 /**
- *
- * @author a1
+ * La clase {@code Task} representa una tarea a realizar en un momento 
+ * determinado del día.
+ * @author kiira
+ * @version 1.0
  */
 public class Task implements Comparable<Task> {
 
+    /** Horario de inicio de la tarea. */
     private Hour initHour;
+    /** Horario de fin de la tarea. */
     private Hour endHour;
+    /** Descripción de la tarea. */
     private String description;
 
+    /**
+     * Inicializa la tarea de manera completa, indicando sus horarios de inicio
+     * y fin, y su descripción.
+     * @param initHour horario de inicio de la tarea.
+     * @param endHour horario de fin de la tarea.
+     * @param description descripción de la tarea.
+     */
     public Task( Hour initHour, Hour endHour, String description ) {
         this.initHour = initHour;
         this.endHour = endHour;
         this.description = description;
     }
 
+    /**
+     * Devuelve el horario de inicio de la tarea.
+     * @return horario de inicio de la tarea.
+     */
     public Hour getInitHour() {
         return initHour;
     }
 
+    /**
+     * Devuelve el horario de fin de la tarea.
+     * @return horario de fin de la tarea.
+     */
     public Hour getEndHour() {
         return endHour;
     }
 
+    /**
+     * Devuelve la descripción de la tarea.
+     * @return descripción de la tarea.
+     */
     public String getDescription() {
         return description;
     }
-    
+
+    /**
+     * {@inheritDoc} 
+     */
     @Override
     public int compareTo( Task task ) {
         return this.initHour.compareTo( task.initHour );
     }
 
+    /**
+     * Inidica si el tiempo durante el cual transcurrirá esta tarea comprende
+     * el horario indicado.
+     * @param hour horario el cual se desea saber si está comprendido en el 
+     * periodo durante el cual transcurrirá la tarea.
+     * @return {@code true} si el horario se encuentra comprendido dentro del
+     * periodo durante el cual transcurrirá la tarea.;
+     * {@code false} en caso contrario.
+     */
     public boolean isInTime( Hour hour ) {
         return this.initHour.compareTo( hour ) < 0
                && this.endHour.compareTo( hour ) > 0;
     }
 
+    /**
+     * Inidica si elhorario indicado por parámetro esta dispuesto "luego" de
+     * finalizada la tarea.
+     * @param hour horario el cual se desea saber si esta dispuesto "luego" de 
+     * finalizada la tarea.
+     * @return {@code true} si el horario esta dispuesto luego de finalizada
+     * la tarea.;
+     * {@code false} en caso contrario.
+     */
     public boolean isAfter( Hour hour ) {
         return this.endHour.compareTo( hour ) <= 0;
     }
 
+    /**
+     * Devuelve la representación de esta tarea en forma de {@code String}, 
+     * mostrando su descripción, hora de inicio y de fin.
+     * @return representación en forma de {@code String} de esta tarea.
+     */
     @Override
     public String toString() {
-        return "Task{" + "initHour=" + initHour + ", endHour=" + endHour + ", description=" + description + '}';
+        return String.format( "Tarea: %s; Inicio: %s; Fin: %s",
+                              this.description, this.initHour,
+                              this.endHour );
     }
-        
+
 }
--- src/core/utils/Hour.java
+++ src/core/utils/Hour.java
@@ -1,64 +1,108 @@
 package core.utils;
+
 /**
- *
- * @author a1
+ * La clase {@code Hour} representa un momento del día, especificado a partir
+ * de la hora del día y la cantidad de minutos.
+ * @author kiira
+ * @version 1.0
  */
 public final class Hour implements Comparable<Hour> {
 
+    /** Cantidad de horas en un día. */
     private static final int HOURS = 24;
+    /** Cantidad de minutos en una hora. */
     private static final int MINS = 60;
+    /** Hora definida para este horario. */
     private int hour;
+    /** Cantidad de minutos pasada la hora, definido para este horario. */
     private int minutes;
 
+    /**
+     * Inicializa el horario a las 00:00
+     */
     public Hour() {
     }
 
+    /**
+     * Inicializa el horario a la hora indicada, sin sumar minutos.
+     * @param hour hora del horario.
+     */
     public Hour( int hour ) {
         this.setHour( hour );
     }
 
+    /**
+     * Inicializa el horario indicando hora y cantida de minutos pasada la hora.
+     * @param hour hora del horario.
+     * @param minutes cantidad de minutos pasado de la hora.
+     */
     public Hour( int hour, int minutes ) {
         this.setHour( hour );
         this.setMinutes( minutes );
     }
 
+    /**
+     * Devuelve la hora de este horario.
+     * @return hora del horario.
+     */
     public int getHour() {
         return hour;
     }
 
+    /**
+     * Permite asignar la hora de este horario.
+     * @param hour hora a asignar.
+     * @throws IllegalArgumentException si el horario esta fuera del rango 
+     * válido de horas (entre 0 y 23).
+     */
     public void setHour( int hour ) {
         if ( hour < 0 || hour >= Hour.HOURS )
-            throw new IllegalArgumentException( 
+            throw new IllegalArgumentException(
                     "La hora debe ser un valor entre 0 y 23." );
-        
+
         this.hour = hour;
     }
 
+    /**
+     * Devuelve la cantidad de minutos pasado de la hora del horario.
+     * @return cantidad de minutos pasados de la hora.
+     */
     public int getMinutes() {
         return minutes;
     }
 
+    /**
+     * Permite asignar la cantidad de minutos pasados de la hora del horaio.
+     * @param minutes cantidad de minutos a asignar.
+     */
     public void setMinutes( int minutes ) {
         if ( minutes < 0 || minutes >= Hour.MINS )
-            throw new IllegalArgumentException( 
+            throw new IllegalArgumentException(
                     "Los minutos deben ser un valor entre 0 y 59." );
-        
+
         this.minutes = minutes;
     }
 
+    /**
+     * {@inheritDoc} 
+     */
     @Override
     public int compareTo( Hour hour ) {
         int res = this.hour - hour.hour;
-        
+
         if ( res == 0 )
             res = this.minutes - hour.minutes;
-        
+
         return res;
     }
 
+    /**
+     * Devuelve la representeación de este horario en el formato HH:MM.
+     * @return representación en forma de {@code String} de este horario.
+     */
     @Override
     public String toString() {
-        return "Hour{" + "hour=" + hour + ", minutes=" + minutes + '}';
+        return String.format( "%02d:%02d", this.hour, this.minutes );
     }
 
 }
