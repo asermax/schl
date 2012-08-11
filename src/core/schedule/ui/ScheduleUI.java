b8a2c5d9030a858cfc48dce731bab91a241aa9bc
asermax <asermax@gmail.com>
2012-08-11 18:43:34 -0300

    Agregada una interfaz correspondiente para cualquier UI de un Schedule. Esta permite al TaskPanel informar a la UI que se desea eliminar su task.

---
+++ src/core/schedule/ui/ScheduleUI.java
@@ -0,0 +1,14 @@
+package core.schedule.ui;
+
+import core.task.Task;
+
+/**
+ * La interfaz {@code ScheduleUI}
+ * @author kiira
+ * @version 1.0, Aug 10, 2012
+ */
+public interface ScheduleUI {
+
+    public void removeTask( Task task );
+
+}
